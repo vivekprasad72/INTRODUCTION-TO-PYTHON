d805af05ea581869ecc045d1dc69b41621d71575
Vivek Prasad <vkjsr12345@example.com>
2026-08-07 00:50:47 +0530

    first commit

---
+++ Fruits.py
@@ -0,0 +1,35 @@
+fruits = {"Apple", "Banana", "Orange", "Mango", "Grapes",
+          "Pineapple", "Papaya", "Guava", "Litchi", "Pomegranate"}
+
+summer_fruits = {"Mango", "Litchi", "Watermelon", "Muskmelon", "Jackfruit"}
+
+winter_fruits = {"Apple", "Orange", "Kiwi", "Pear", "Strawberry"}
+
+print("Fruits:", fruits)
+print("Summer Fruits:", summer_fruits)
+print("Winter Fruits:", winter_fruits)
+
+print("\nFruits present in Fruits and Winter Fruits:")
+print(fruits & winter_fruits)
+
+print("\nFruits present only in Summer Fruits but not in Fruits:")
+print(summer_fruits - fruits)
+
+print("\nFruits present in Summer and Winter Fruits but not in Fruits:")
+print((summer_fruits & winter_fruits) - fruits)
+
+if "Orange" in fruits:
+    print("\nOrange is present in Fruits.")
+else:
+    print("\nOrange is not present in Fruits.")
+
+print("\nPineapple is present in:")
+
+if "Pineapple" in fruits:
+    print("Fruits")
+
+if "Pineapple" in summer_fruits:
+    print("Summer Fruits")
+
+if "Pineapple" in winter_fruits:
+    print("Winter Fruits")
