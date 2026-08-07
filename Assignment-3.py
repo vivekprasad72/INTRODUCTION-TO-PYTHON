b07a8cee6cca11e22628d442465c5d23739d94a2
Vivek Prasad <vkjsr12345@example.com>
2026-08-08 01:57:47 +0530

    First commit

--- Assignment-3.py
+++ Assignment-3.py
@@ -1,4 +1,3 @@
-# 20 employee names
 emp = (
     "Ankit", "Rahul", "Sanjeev", "Amit", "Janvi",
     "Rahul", "Rahul", "Ankit", "Karan", "Kaushik",
@@ -6,23 +5,18 @@ emp = (
     "Ayashi", "Sumit", "Rahul", "Sanjeev", "Amit"
 )
 
-# i) Name and frequency
 print("Frequencies:")
 for name in set(emp):
     print(name, emp.count(name))
 
-# ii) Distinct names
 names = set(emp)
 print("\nDistinct names:", names)
 print("Total distinct:", len(names))
 
-# iii) Most frequent employee
 print("\nMost frequent:", max(names, key=emp.count))
 
-# iv) Sorted tuple
 print("\nSorted:", tuple(sorted(emp)))
 
-# v) Search employee
 name = input("\nEnter name to search: ")
 if name in emp:
     print("Found!")
