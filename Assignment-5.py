aa68cd4d842203299341fb964846996afdc3c7f3
Vivek Prasad <vkjsr12345@example.com>
2026-08-14 08:11:57 +0530

    Assignment-5

---
+++ Assignment-5.py
@@ -0,0 +1,13 @@
+employees = {
+    "E1": {"Name": "Amit", "Dept": "HR", "Salary": 30000},
+    "E2": {"Name": "Rahul", "Dept": "IT", "Salary": 45000},
+    "E3": {"Name": "Neha", "Dept": "Finance", "Salary": 40000},
+    "E4": {"Name": "Priya", "Dept": "Marketing", "Salary": 35000},
+    "E5": {"Name": "Rohan", "Dept": "Sales", "Salary": 32000}
+}
+
+print("Employee E1 Record:")
+print(employees["E1"])
+
+print("\nDepartment of E4:")
+print(employees["E4"]["Dept"])
